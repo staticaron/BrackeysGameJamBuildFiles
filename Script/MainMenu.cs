29f00e8d4ed61c4ee0b0f77a416945bc81e09a84
Devanshu Mishra <mishradev1910@gmail.com>
2020-07-31 07:38:02 +0530

    Game Done

---
+++ Script/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			SceneManager.LoadScene(2);
+		}
+		else if (Input.GetKeyDown(KeyCode.Q))
+		{
+			Application.Quit();
+		}
+		else if (Input.GetKeyDown(KeyCode.X))
+		{
+			SceneManager.LoadScene(1);
+		}
+		else if (Input.GetKeyDown(KeyCode.M))
+		{
+			SceneManager.LoadScene(0);
+		}
+	}
+}
